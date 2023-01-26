8b993881d9a50b853a59c71fc583f56f13b96e5e
Petr Spěvák <petr@spevak.eu>
2023-01-26 16:55:28 +0100

    verzovani API - prefixnuti API rout '/v1'

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 
-const planetsRouter = require('./routes/planets/planets.router');
+const apiRouter = require('./routes/api.router');
 const launchesRouter = require('./routes/launches/launches.router');
 
 const app = express();
@@ -17,8 +17,7 @@ app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.use(planetsRouter);
-app.use('/launches', launchesRouter);
+app.use('/v1', apiRouter);
 
 // /* - routa, ktera se nechytla na predchozich, se preda Reacti aplikaci
 app.get('/*', (req, res) => {
