bed31eed6a172f22a4418d5e5b048c17fca22e91
Petr Spěvák <petr@spevak.eu>
2023-01-26 11:47:58 +0100

    presun pripojeni k databazi do 'services'

--- server/src/server.js
+++ server/src/server.js
@@ -1,21 +1,17 @@
 const http = require('http');
-const mongoose = require('mongoose');
+const { mongoConnect } = require('./services/mongo');
 
 const app = require('./app');
 const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT ?? 8000;
 
-const MONGO_URL = 'mongodb+srv://nasa-api:l4SoxK6dt7tyx1dw@tutorialcluster.uvyzlbc.mongodb.net/nasaDB?retryWrites=true&w=majority';
 
 const server = http.createServer(app);
 
-mongoose.connection.once('open', () => {
-    console.log('MongoDB connection is ready.');
-});
 
 async function startServer() {
-    await mongoose.connect(MONGO_URL);
+    await mongoConnect();
     await loadPlanetsData();
     server.listen(8000, () => console.log(`Server runs on port ${PORT}`));
 }
---
+++ server/src/services/mongo.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const MONGO_URL = 'mongodb+srv://nasa-api:l4SoxK6dt7tyx1dw@tutorialcluster.uvyzlbc.mongodb.net/nasaDB?retryWrites=true&w=majority';
+
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection is ready.');
+});
+
+mongoose.connection.on('error', err => {
+    console.error(err);
+});
+
+mongoose.connect(MONGO_URL);
+
+async function mongoConnect() {
+    await mongoose.connect(MONGO_URL);
+}
+
+module.exports = {
+    mongoConnect,
+};
+
